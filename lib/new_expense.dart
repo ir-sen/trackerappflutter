baffe1cff5487efbbc4a1c6caa21a6f3638040b6
irsen <irsen.kan@bk.ru>
2024-05-17 10:34:48 +0900

    add dropDown Button and save expense

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:trackerapp/models/expense.dart';  
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
+
+
   
   @override
   State<NewExpense> createState() {
@@ -19,6 +23,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate; 
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now(); 
@@ -38,6 +43,43 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+
+  void _submitExpenseData() {
+    // tryParse('Hello') => null, tryParse('1.12') => 1.12
+    final enterdAmount = double.tryParse(_amountController.text);
+    final amoutIsInvalid = enterdAmount == null || enterdAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty || amoutIsInvalid || _selectedDate == null) {
+  
+      // show error
+      showDialog(context: context, builder: (ctx) =>
+        AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text('Please make sure a valid title, amount, date and category was entered.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              }, child: const Text('Okey'),
+            )
+          ]
+        )
+        );
+        return;
+    }
+
+    widget.onAddExpense(Expense(
+      title: _titleController.text,
+      anount: enterdAmount,
+      date: _selectedDate!,
+      category: _selectedCategory,
+      ));
+
+      
+    Navigator.pop(context);
+    
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -55,7 +97,8 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(children: [
         // text field for writting text 
         TextField(
@@ -97,7 +140,7 @@ class _NewExpenseState extends State<NewExpense> {
                 ? 'No date selected'
                 : formatter.format(_selectedDate!)
               ),
-              
+
               IconButton(
                 onPressed: _presentDatePicker, 
                 icon: const Icon(Icons.calendar_month),
@@ -108,10 +151,34 @@ class _NewExpenseState extends State<NewExpense> {
           ],
         ),
 
-        const SizedBox(height: 10,),
+        const SizedBox(height: 16,),
 
         Row(children: [
           
+          DropdownButton(
+            value: _selectedCategory,
+            items: Category.values.map(
+            (category) => DropdownMenuItem(
+              value: category,
+              child: Text(
+                category.name.toUpperCase(),
+                ),
+              ) 
+            ).toList(),
+
+            onChanged: (value) {
+              
+              if (value == null) {
+                return;
+              }
+
+              setState(() {
+                _selectedCategory = value;
+              });
+  
+            }
+          ),
+
           TextButton(
             onPressed: () {
               // this command remore this overlate
@@ -120,10 +187,10 @@ class _NewExpenseState extends State<NewExpense> {
             child: const Text('Cancel'),
           ),
 
-          ElevatedButton(onPressed: () {
-            print(_titleController.text);
-            print(_amountController.text);
-          }, child: const Text('Save Expense')),
+          ElevatedButton(
+            onPressed: _submitExpenseData,
+            child: const Text('Save Expense')
+            ),
           // const SizedBox(width: 10,),
 
 
